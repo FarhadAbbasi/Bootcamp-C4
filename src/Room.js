bfff4bf01a22711b08d233a8b31ca2a2b49a9c0e
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2024-11-05 22:58:16 +0500

    Added toggle  buttons using react 'useState' hook

--- src/Room.js
+++ src/Room.js
@@ -6,18 +6,24 @@ function Room() {
   let state = useState(false) ;
   let isLit = state[0] ;
   let setLit = state[1] ;
+  // console.log("State = ", state) ;  
 
   function UpdateLit() {
-    console.log ("Button Clicked");
+    console.log ("Lit Button Clicked");
     setLit(!isLit) ; 
   }
 
- // console.log("State f = ", state) ;  
+  let [age, setage] = useState(25) ;
+  const UpdateAge = ()=> ( setage(--age + 2) )  //use curly bracket for multiple lines as usual
+
+  let [level, setLevel] = useState(1) ;
 
   return (
     <div> 
-          This Room is  {isLit? "Lit" : "Dark" } <br/> 
-        <button onClick={UpdateLit} color ="#DDD">Toggle Light </button>
+          This Room is  {isLit? "Lit" : "Dark" } <br/>Age = {age} <br/>Level  = {level} <br/>
+        <button onClick={UpdateLit} > Toggle Light </button> <span/>
+        <button onClick={UpdateAge} > Increment Age </button> <span/>
+        <button onClick={ ()=> setLevel(++level) }> Increment Level </button> <span/>
     </div>
   );
 }
